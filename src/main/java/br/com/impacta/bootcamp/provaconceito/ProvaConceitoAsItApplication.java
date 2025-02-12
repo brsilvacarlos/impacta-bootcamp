a68fefda11f41b2043dda9349c95a65ab767862b
Carlos Silva <brsilvacarlos@gmail.com>
2025-02-11 21:04:35 -0300

    Tirando devtools; add logs de acesso das páginas de teste

--- src/main/java/br/com/impacta/bootcamp/provaconceito/ProvaConceitoAsItApplication.java
+++ src/main/java/br/com/impacta/bootcamp/provaconceito/ProvaConceitoAsItApplication.java
@@ -8,6 +8,10 @@ public class ProvaConceitoAsItApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProvaConceitoAsItApplication.class, args);
+
+		System.out.println("Páginas do teste:");
+		System.out.println("As it: http://localhost:8081/asit");
+		System.out.println("To be: http://localhost:8081/tobe");
 	}
 
 }
